cfa41a9f82cf03b775e7cb4d32a816a1ace79fb6
Luke Palmer <lrpalmer@gmail.com>
2011-06-03 01:02:46 -0600

    Converted all tokens to Token combinators in the grammar.

--- grammars/javascript.js
+++ grammars/javascript.js
@@ -2,45 +2,54 @@
 var floating_point_regexp = /^[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/;
 // End CodeCatalog Snippet
 
-// CodeCatalog Snippet http://www.codecatalog.net/357/2/
+// CodeCatalog Snippet http://www.codecatalog.net/357/3/
 var escape_for_regexp = function(text) {
-    return text.replace(/[][\\^$*+?{}\.()|]/g, function(x) { '\\' + x });
+    return text.replace(/[\]\[\\^$*+?{}\.()|]/g, function(x) { return '\\' + x });
 };
 // End CodeCatalog Snippet
 
 var Token = {
     space_after: function(text) {
         return {
-            pattern: new RegExp('^' + escape_for_regexp(text) + '\\s*'),
+            pattern: escape_for_regexp(text) + '\\s*',
             text: text + ' '
+        }
+    },
+    rx: function(rx) {
+        return {
+            pattern: rx.source
+        }
+    },
+    string: function(text) {
+        return {
+            pattern: escape_for_regexp(text)
         }
     },
 	no_space_after_rx: function(rx) {
 		return {
-			pattern: new RegExp('^(' + rx.source + ')\\s*'),
+			pattern: '(' + rx.source + ')\\s*',
 			text: function(m) { return m[1] }
 		}
 	},
 	no_space_before_rx: function(rx) {
 		return {
-			pattern: new RegExp('^\\s*(' + rx.source + ')'),
+			pattern: '\\s*(' + rx.source + ')',
 			text: function(m) { return m[1] }
 		}
 	},
     space_padded_rx: function(rx) {
         return {
-            pattern: new RegExp('^\\s*(' + rx.source + ')\\s*'),
+            pattern: '\\s*(' + rx.source + ')\\s*',
             text: function(m) { return ' ' + m[1] + ' ' }
         }
     },
 	newline_after: function(text) {
 		return {
-			pattern: new RegExp('^' + escape_for_regexp(text) + '\\s*'),
+			pattern: escape_for_regexp(text) + '\\s*',
 			text: text + '\n'
 		}
 	},
 };
-
 
 var javascript_grammar = {
     program: [ [ 'stmts' ] ],
@@ -64,12 +73,12 @@ var javascript_grammar = {
     return_stmt: [ [ 'return_keyword', 'expr' ] ],
     void_stmt: [ [ 'void_keyword', 'expr' ] ],
 
-    function_stmt: [ [ 'function_keyword', 'identifier', /^\(/, 'arg_list', /^\)/, 'block' ] ],
+    function_stmt: [ [ 'function_keyword', 'identifier', Token.string('('), 'arg_list', Token.string(')'), 'block' ] ],
 
     // TODO what is the syntactic class of the first clause?
-    for_stmt: [ [ 'for_keyword', /^\(/, 'stmt', /^;/, 'expr', /^;/, 'expr', /^\)/, 'stmt' ] ],
-    foreach_stmt: [ [ 'for_keyword', /^\(/, 'foreach_var_decl', /^\s+/, 'in_keyword', 'expr', /^\)/, 'stmt' ] ],
-    if_stmt: [ [ 'if_keyword', /^\(/, 'expr', /^\)/, 'stmt', 'else_clause' ] ],
+    for_stmt: [ [ 'for_keyword', Token.string('('), 'stmt', Token.string(')'), 'expr', Token.string(';'), 'expr', Token.string(')'), 'stmt' ] ],
+    foreach_stmt: [ [ 'for_keyword', Token.string('('), 'foreach_var_decl', Token.rx(/\s+/), 'in_keyword', 'expr', Token.string(')'), 'stmt' ] ],
+    if_stmt: [ [ 'if_keyword', Token.string('('), 'expr', Token.string(')'), 'stmt', 'else_clause' ] ],
     else_clause: [ [ ], [ 'else_keyword', 'stmt' ] ],
 
     foreach_var_decl: [ [ 'var_keyword', 'identifier' ],
@@ -82,7 +91,7 @@ var javascript_grammar = {
 
     initializer: [ [ 'identifier', Token.space_padded_rx(/=/), 'expr' ] ],
 
-    identifier: [ [ /^[a-zA-Z_$][\w$]*/ ] ],
+    identifier: [ [ Token.rx(/[a-zA-Z_$][\w$]*/) ] ],
 
     expr: [ [ 'assignment_expr' ] ],
     
@@ -100,8 +109,8 @@ var javascript_grammar = {
     unary_expr: [ [ 'atomic_expr' ],
                   [ 'prefix_operator', 'unary_expr' ] ],
 
-    prefix_operator: [ [ Token.no_space_after_rx(/^(\+\+|--|\+|-|!)/) ] ],
-    postfix_operator: [ [ Token.no_space_before_rx(/^(\+\+|--)/) ] ],
+    prefix_operator: [ [ Token.no_space_after_rx(/(\+\+|--|\+|-|!)/) ] ],
+    postfix_operator: [ [ Token.no_space_before_rx(/(\+\+|--)/) ] ],
 
     atomic_expr: [ [ 'literal' ],
                    [ 'function_expr' ],
@@ -110,42 +119,42 @@ var javascript_grammar = {
                    [ 'index_expr' ],
                    [ 'funcall_expr' ],
                    [ 'variable' ],
-                   [ /^\(/, 'expr', /^\)/ ] ],
+                   [ Token.string('('), 'expr', Token.string(')') ] ],
 
-    literal: [ [ floating_point_regexp ],
-               [ /^"/, 'dq_string', /^"/ ],
-               [ /^'/, 'sq_string', /^'/ ],
-               [ /\//, 'slash_string', /\// ] ],
+    literal: [ [ Token.rx(floating_point_regexp) ],
+               [ Token.string('"'), 'dq_string', Token.string('"') ],
+               [ Token.string("'"), 'sq_string', Token.string("'") ],
+               [ Token.string('/'), 'slash_string', Token.string('/') ] ],
 
-    dq_string: [ [ /^[^"\\]*(\\.[^"\\]*)*/ ] ],
-    sq_string: [ [ /^[^'\\]*(\\.[^'\\]*)*/ ] ],
-    slash_string: [ [ /^[^\/\\]*(\\.[^\/\\]*)*/ ] ],
+    dq_string: [ [ Token.rx(/^[^"\\]*(\\.[^"\\]*)*/) ] ],
+    sq_string: [ [ Token.rx(/^[^'\\]*(\\.[^'\\]*)*/) ] ],
+    slash_string: [ [ Token.rx(/^[^\/\\]*(\\.[^\/\\]*)*/) ] ],
     
-    function_expr: [ [ 'function_keyword', /^\(/, 'arg_list', /^\)/, 'block' ] ],
+    function_expr: [ [ 'function_keyword', Token.string('('), 'arg_list', Token.string(')'), 'block' ] ],
     arg_list: [ [ ], [ 'identifier', 'more_arg_list' ] ],
     more_arg_list: [ [ ], [ Token.space_after(','), 'identifier', 'more_arg_list' ] ],
 
-    block: [ [ /^\{/, 'stmts', /^\}/ ] ],
+    block: [ [ Token.string('{'), 'stmts', Token.string('}') ] ],
 
-    array_expr: [ [ /^\[/, 'expr_list', /^\]/ ] ],
+    array_expr: [ [ Token.string('['), 'expr_list', Token.string(']') ] ],
     
     expr_list: [ [ ], [ 'expr', 'more_expr_list' ] ],
     more_expr_list: [ [ ], [ Token.space_after(','), 'expr', 'more_expr_list' ] ],
 
-    object_expr: [ [ /^\{/, 'property_list', /^\}/ ] ],
+    object_expr: [ [ Token.string('{'), 'property_list', Token.string('}') ] ],
 
     property_list: [ [ ], [ 'property_kv', 'more_property_list' ] ],
-    more_property_list: [ [ ], [ /^,/, 'property_kv', 'more_property_list' ] ],
+    more_property_list: [ [ ], [ Token.string(','), 'property_kv', 'more_property_list' ] ],
 
-    property_kv: [ [ 'property_key', /^:/, 'expr' ] ],
+    property_kv: [ [ 'property_key', Token.string(':'), 'expr' ] ],
 
     property_key: [ [ 'identifier' ],
                     [ 'literal' ] ],   // not exactly right.  eg. regexes can't be keys
 
-    index_expr: [ [ 'atomic_expr', /^\[/, 'expr', /^\]/ ],
-                  [ 'atomic_expr', /^\./, 'identifier' ] ],
+    index_expr: [ [ 'atomic_expr', Token.string('['), 'expr', Token.string(']') ],
+                  [ 'atomic_expr', Token.string('.'), 'identifier' ] ],
 
-    funcall_expr: [ [ 'atomic_expr', /^\(/, 'expr_list', /^\)/ ] ],
+    funcall_expr: [ [ 'atomic_expr', Token.string('('), 'expr_list', Token.string(')') ] ],
 
     variable: [ [ 'identifier' ] ],
 
